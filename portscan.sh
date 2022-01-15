efbefb76194957ba85a34ad68aa5f6e4ad6dd01e
sandeep-vatada <97786673+sandeep-vatada@users.noreply.github.com>
2022-01-15 06:43:42 -0800

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -11,16 +11,13 @@ echo "Syntax : ./portscan.sh <ip address>"
 
 else
 nmap -T4 -Pn -p- $1 > Result.txt; #saved output into Result.txt file
-
+#Scanning using nmap tool providing Maximum threads i.e..,T4 and the results are piped into Result.txt
 cat Result.txt | grep "open" | cut -d " " -f 1 | tr -d "/tcp" | tr "\n" "," | sed 's/.$//' > openports.txt;
-
-
+#To open the result and grep only open ports which cut the space between them with one field 
 nmap -T4 -Pn  -p $(cat openports.txt) -A $1 > finalresults.txt ;
-
 rm -rf Result.txt
-
 rm -rf openports.txt
-
+#To remove the additional files using rm -rf "filename"
 
 fi
 
